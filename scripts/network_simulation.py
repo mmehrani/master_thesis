0866c25b4b8b2fc73775327f322bd70dcc92df96
Mohsen Mehrani <m.mehrani14@gmail.com>
2021-01-31 09:55:51 +0330

    problem with 10**4 neurons fixed
    the problem where the way we defined spiking records as a boolean array

--- scripts/network_simulation.py
+++ scripts/network_simulation.py
@@ -19,7 +19,7 @@ class Kuramoto_neural_network:
     def _retarded_spikes_record(self,step):
         
         if step > self.delay_step: 
-            return np.sum(self.spiking_records[step - self.delay_step])
+            return self.spiking_records[step - self.delay_step]
         else:
             return 0
     
@@ -34,12 +34,11 @@ class Kuramoto_neural_network:
         potentail_arr = np.zeros(self.num_neurons,dtype = float)
         random_input = np.random.uniform(*random_input_span,size = self.num_neurons)
         
-        self.spiking_records = np.array( np.zeros((total_steps,self.num_neurons)), dtype = bool )
+        self.spiking_records = np.array( np.zeros(total_steps) )
         
         for i in tqdm(range(total_steps),desc = 'network dynamic'):
-            potentail_arr = potentail_arr + (random_input - potentail_arr)*time_step \
-                                - (self.g / self.num_neurons )*self._retarded_spikes_record(i)
-            self.spiking_records[i] = potentail_arr > 1
+            potentail_arr = potentail_arr + (random_input - potentail_arr)*time_step - (self.g / self.num_neurons )*self._retarded_spikes_record(i)
+            self.spiking_records[i] = np.sum( potentail_arr > 1 )
             reset_func = lambda v: v-1 if v>1 else v
             vfunc = np.vectorize(reset_func)
             potentail_arr = vfunc(potentail_arr)
@@ -56,8 +55,7 @@ class Kuramoto_neural_network:
         e_arr = np.zeros(self.total_steps)
         
         for i in range(0,self.total_steps-1):
-            m_arr[i+1] = m_arr[i] + self.time_step* \
-                        ( -alpha*m_arr[i] ) + (alpha**2 / self.num_neurons ) * self._retarded_spikes_record(i)
+            m_arr[i+1] = m_arr[i] + self.time_step* ( -alpha*m_arr[i] ) + (alpha**2 / self.num_neurons ) * self._retarded_spikes_record(i)
             e_arr[i+1] = e_arr[i] + self.time_step*( m_arr[i] - alpha*e_arr[i] )
         
         self.effective_field_array = e_arr
@@ -70,7 +68,7 @@ class Kuramoto_neural_network:
     
     pass
 
-# sample_model = Kuramoto_neural_network(num_neurons=100,g=5)
-# sample_model.ignite(total_time = 1000)
-# sample_model.compute_effective_field( alpha = 20)
-# sample_model.report_sigma()
+sample_model = Kuramoto_neural_network(num_neurons=10000,g=0.1)
+sample_model.ignite(total_time = 1000)
+sample_model.compute_effective_field( alpha = 20)
+sigma = sample_model.report_sigma()
