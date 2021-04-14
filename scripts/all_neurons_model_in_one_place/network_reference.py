9a356543af4e37ca03696135ff966089b1d57b1b
Mohsen Mehrani <m.mehrani14@gmail.com>
2021-04-14 19:18:22 +0430

    neural models can be used as networks engines

---
+++ scripts/all_neurons_model_in_one_place/network_reference.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 11 17:02:33 2021
+
+@author: mohsen
+"""
+
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from neurons_engines import Rotational_neural_network, Kuramoto_neural_network
+
+network_engine_class = Kuramoto_neural_network
+
+class Network_of_neurons(network_engine_class):
+    def __init__(self,num_neurons,g,alpha = 20):
+        self.num_neurons = num_neurons
+        self.g = g
+        self.alpha = alpha
+        return
+    
+    def _retarded_spikes_record(self,step):
+        if step > self.delay_step: 
+            return self.spiking_records[step - self.delay_step]
+        else:
+            return 0
+    
+    def ignite(self,random_input_span,total_time,time_step = 0.01,delay_time = 0.1):
+        
+        # random_input_span = (3.5,13.5)
+        
+        total_steps = int(total_time/time_step)
+        time_span = np.arange(0,total_time,time_step)
+        
+        self.total_time = total_time
+        self.total_steps = total_steps
+        self.time_step = time_step
+        self.delay_step = int(delay_time/time_step)
+        
+        self.potentail_arr = np.zeros(self.num_neurons,dtype = float)
+        self.m_arr = np.zeros(total_steps)
+        self.e_arr = np.zeros(total_steps)
+        self.random_input = np.random.uniform(*random_input_span,size = self.num_neurons)
+        
+        self.amin_saman_param = np.zeros( total_steps )
+        self.spiking_records = np.zeros(total_steps)
+        
+        self._set_cornometers()
+        
+        for i in tqdm( range(total_steps - 1),desc = 'network dynamic' ):
+            self._march_on(i)
+            self._update_cornometers()
+            
+        return
+    
+    def _set_cornometers(self):
+        """
+        These cornometers are going to record mean spikes intervals for every neuron.
+
+        """
+        self.spiking_cornometer = np.zeros(self.num_neurons)
+        self.total_spikes_intervals = np.zeros(self.num_neurons)
+        self.total_spikes_num = np.zeros(self.num_neurons)
+        return
+    
+    def _update_cornometers(self):
+        """
+        Set lapsed time for every neuron which spiked. In addition, let other
+        cornometers to keep on ticking.
+
+        """
+        self.total_spikes_num = self.total_spikes_num + self.spike_mask
+        self.total_spikes_intervals = self.total_spikes_intervals + self.spiking_cornometer * self.spike_mask #add to spike lapsed time
+        self.spiking_cornometer = self.spiking_cornometer * (self.spike_mask-1)*(-1) #reset the spikied cornos
+        self.spiking_cornometer = self.spiking_cornometer + (self.spike_mask-1)*(-1) #update the not spikied ones
+        return
+
+    def report_spikies_period_mean(self):
+        """
+        Wrap up the results recorded by cornometers.
+
+        Returns
+        -------
+        spikies_period_mean
+            an array of total neurons number which shows the mean period of spiking.
+
+        """
+        self.spikies_period_mean = self.total_spikes_intervals * self.time_step / self.total_spikes_num
+        return self.spikies_period_mean
+
+
+    def report_sigma(self):
+        sigma = np.std( self.e_arr )
+        return sigma
+
+    pass
+
+
+
+
+
+# sample_model = Kuramoto_neural_network(num_neurons=10000,g=5)
+# sample_model.ignite(total_time = 1000)
+# sample_model.compute_effective_field( alpha = 20)
+# sigma = sample_model.report_sigma()
+    
+sample_model = Network_of_neurons(num_neurons= 1000, g = 5)
+sample_model.ignite(random_input_span = (3.5,13.5),total_time = 1000)
+sigma = sample_model.report_sigma()
+spikies_period_mean = sample_model.report_spikies_period_mean()
---
+++ scripts/all_neurons_model_in_one_place/neurons_engines.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 14 19:07:00 2021
+
+@author: mohsen
+"""
+
+import numpy as np
+
+class Rotational_neural_network:
+    
+    def __init__(self,num_neurons,g):
+        self.num_neurons = num_neurons
+        self.g = g
+
+    
+        return
+    
+    def _march_on(self,i):
+        """
+        Push the network forward in time.
+
+        Parameters
+        ----------
+        i : integer
+            This is the steps number during the dynamic
+
+        Returns
+        -------
+        None.
+
+        """
+        self.potentail_arr = self.potentail_arr + (self.random_input - np.cos(self.potentail_arr) - self.g * self.e_arr[i] )*self.time_step
+        self.m_arr[i+1] = self.m_arr[i] + self.time_step*( -self.alpha*self.m_arr[i] ) + ( (self.alpha**2)/self.num_neurons ) *self._retarded_spikes_record(i) 
+        self.e_arr[i+1] = self.e_arr[i] + self.time_step*( self.m_arr[i] - self.alpha*self.e_arr[i] )
+        
+        #here we should spot the spiking neurons.
+        self.spike_mask = self.potentail_arr > np.pi
+        self.spiking_records[i] = np.sum( self.spike_mask )
+        self.potentail_arr = self.potentail_arr - 2 * np.pi * self.spike_mask
+        
+        self._record_amin_saman_param(i)
+
+        return
+    
+    def _record_amin_saman_param(self,step):
+        left_handed_neurons = np.cos(self.potentail_arr) < 0
+        self.amin_saman_param[step] = np.mean( np.sin(self.potentail_arr[left_handed_neurons]) )**2
+        return
+    
+    
+    def report_sync_parameter(self,last_steps_period = 1000):
+        if self.total_steps < 1000: last_steps_period = self.total_steps
+        return np.mean( self.amin_saman_param[-last_steps_period:] )
+
+    
+    pass
+
+
+
+class Kuramoto_neural_network:
+    
+    def __init__(self,num_neurons,g,alpha = 20):
+        self.num_neurons = num_neurons
+        self.g = g
+        self.alpha = alpha
+        return
+
+    def _march_on(self,i):
+        """
+        Push the network forward in time.
+
+        Parameters
+        ----------
+        i : integer
+            This is the steps number during the dynamic
+
+        Returns
+        -------
+        None.
+
+        """
+        self.potentail_arr = self.potentail_arr + (self.random_input - self.potentail_arr)*self.time_step - (self.g / self.num_neurons )*self._retarded_spikes_record(i)
+        
+        #here we should spot the spiking neurons.
+        self.spike_mask = self.potentail_arr > 1
+        self.spiking_records[i] = np.sum( self.spike_mask )
+        self.potentail_arr = self.potentail_arr - 1*(self.potentail_arr > 1)
+        
+        self.m_arr[i+1] = self.m_arr[i] + self.time_step*( -self.alpha*self.m_arr[i] ) + ( (self.alpha**2)/self.num_neurons ) *self._retarded_spikes_record(i) 
+        self.e_arr[i+1] = self.e_arr[i] + self.time_step*( self.m_arr[i] - self.alpha*self.e_arr[i] )
+        
+        
+        return
+        
+    pass
